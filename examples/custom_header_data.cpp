21bf3fa463c8d970626ec69223522f4a4ac27b7f
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-05-13 15:46:15 +0200

    bugfix

--- examples/custom_header_data.cpp
+++ examples/custom_header_data.cpp
@@ -63,7 +63,7 @@ std::string HeaderDataToString(const CustomHeaderData header) {
 // hepler function to crash on result fail
 void DieOnFail(const dnet::Result res, CustomConnection& con) {
   if (res == dnet::Result::kFail) {
-    dlog::tinfo("die_on_fail", "failed with error [{}]",
+    DLOG_INFO("die_on_fail", "failed with error [{}]",
                 con.LastErrorToString());
     std::cout << std::endl;  // flush
     exit(0);
@@ -76,11 +76,11 @@ void DieOnFail(const dnet::Result res, CustomConnection& con) {
 
 void Client(const u16 port, const char* ip) {
   // connect to the server
-  dlog::tinfo("client", "connecting to {}:{}", ip, port);
+  DLOG_INFO("client", "connecting to {}:{}", ip, port);
   CustomConnection client{};
   dnet::Result res = client.Connect(std::string(ip), port);
   DieOnFail(res, client);
-  dlog::tinfo("client", "connected");
+  DLOG_INFO("client", "connected");
 
   // sent handshake with empty payload
   {
@@ -90,10 +90,10 @@ void Client(const u16 port, const char* ip) {
     header_data.id = 1337;  // why not
 
     // send the data
-    dlog::tinfo("client", "writing handshake");
+    DLOG_INFO("client", "writing handshake");
     res = client.Write(header_data, payload);
     DieOnFail(res, client);
-    dlog::tinfo("client", "wrote [{}|]", HeaderDataToString(header_data));
+    DLOG_INFO("client", "wrote [{}|]", HeaderDataToString(header_data));
   }
 
   // send echo request - ping
@@ -106,20 +106,20 @@ void Client(const u16 port, const char* ip) {
     header_data.id = 55;
 
     // send the data
-    dlog::tinfo("client", "writing data");
+    DLOG_INFO("client", "writing data");
     res = client.Write(header_data, payload);
     DieOnFail(res, client);
-    dlog::tinfo("client", "wrote [{}|{}]", HeaderDataToString(header_data),
+    DLOG_INFO("client", "wrote [{}|{}]", HeaderDataToString(header_data),
                 msg);
   }
 
   // wait for echo response - pong
   {
     std::vector<u8> payload{};
-    dlog::tinfo("client", "waiting for response");
+    DLOG_INFO("client", "waiting for response");
     auto [read_res, header_data] = client.Read(payload);
     DieOnFail(read_res, client);
-    dlog::tinfo("client", "read [{}|{}]", HeaderDataToString(header_data),
+    DLOG_INFO("client", "read [{}|{}]", HeaderDataToString(header_data),
                 std::string(payload.begin(), payload.end()));
   }
 
@@ -132,10 +132,10 @@ void Client(const u16 port, const char* ip) {
     header_data.id = 0;
 
     // send the data
-    dlog::tinfo("client", "writing data");
+    DLOG_INFO("client", "writing data");
     res = client.Write(header_data, payload);
     DieOnFail(res, client);
-    dlog::tinfo("client", "wrote [{}|]", HeaderDataToString(header_data));
+    DLOG_INFO("client", "wrote [{}|]", HeaderDataToString(header_data));
   }
 
   client.Disconnect();
@@ -160,11 +160,11 @@ void Serve(CustomConnection client, bool& run_server) {
 
   // wait for handshake packet
   {
-    dlog::tinfo("serve", "[port:{}] waiting for handshake", peer_port);
+    DLOG_INFO("serve", "[port:{}] waiting for handshake", peer_port);
     auto [res, header_data] = client.Read(payload);
     if (res == dnet::Result::kSuccess &&
         header_data.type == PacketType::kHandshake) {
-      dlog::tinfo("client", "[port:{}] read [{}|{}] - handshake success",
+      DLOG_INFO("client", "[port:{}] read [{}|{}] - handshake success",
                   peer_port, HeaderDataToString(header_data),
                   std::string(payload.begin(), payload.end()));
     } else {
@@ -176,18 +176,18 @@ void Serve(CustomConnection client, bool& run_server) {
 
   while (run) {
     // wait for packet
-    dlog::tinfo("serve", "[port:{}] waiting for packet", peer_port);
+    DLOG_INFO("serve", "[port:{}] waiting for packet", peer_port);
     auto [res, header_data] = client.Read(payload);
     if (res == dnet::Result::kSuccess) {
       // print packet
-      dlog::tinfo("serve", "[port:{}] read [{}|{}]", peer_port,
+      DLOG_INFO("serve", "[port:{}] read [{}|{}]", peer_port,
                   HeaderDataToString(header_data),
                   std::string(payload.begin(), payload.end()));
 
       // respond depending on packet type
       switch (header_data.type) {
         case PacketType::kHandshake: {
-          dlog::tinfo("serve", "[port:{}] handshake noop at this point",
+          DLOG_INFO("serve", "[port:{}] handshake noop at this point",
                       peer_port);
           break;
         }
@@ -196,17 +196,17 @@ void Serve(CustomConnection client, bool& run_server) {
           header_data_pong.type = PacketType::kPong;
           header_data_pong.id = 0;
 
-          dlog::tinfo("serve", "[port:{}] echoing back the message", peer_port);
+          DLOG_INFO("serve", "[port:{}] echoing back the message", peer_port);
           res = client.Write(header_data_pong, payload);
           if (res != dnet::Result::kSuccess) {
-            dlog::tinfo("serve", "[port:{}] failed to write with error [{}]",
+            DLOG_INFO("serve", "[port:{}] failed to write with error [{}]",
                         peer_port, client.LastErrorToString());
             run = false;
           }
           break;
         }
         case PacketType::kPong: {
-          dlog::tinfo("serve", "[port:{}] pong not implemented", peer_port);
+          DLOG_INFO("serve", "[port:{}] pong not implemented", peer_port);
           break;
         }
         case PacketType::kShutdown: {
@@ -228,14 +228,14 @@ void Serve(CustomConnection client, bool& run_server) {
 // listen for connections on main thread
 void RunServer(const u16 port) {
   // start the server
-  dlog::tinfo("server", "starting server");
+  DLOG_INFO("server", "starting server");
   CustomConnection server{};
   dnet::Result res = server.StartServer(port);
   DieOnFail(res, server);
-  dlog::tinfo("server", "server running @ {}:{}\n",
+  DLOG_INFO("server", "server running @ {}:{}\n",
               server.GetIp().value_or("error"), server.GetPort().value_or(0));
 
-  dlog::tinfo("server", "waiting for client\n");
+  DLOG_INFO("server", "waiting for client\n");
   bool run = true;
   while (run) {
     // wait for client to connect
@@ -245,17 +245,17 @@ void RunServer(const u16 port) {
         const auto [got_peer, peer_ip, peer_port] =
             maybe_client.value().GetPeer();
         if (got_peer == dnet::Result::kSuccess) {
-          dlog::tinfo("server", "new client from {}:{}\n", peer_ip, peer_port);
+          DLOG_INFO("server", "new client from {}:{}\n", peer_ip, peer_port);
 
           // handle the client on a seperate thread
           std::thread t(Serve, std::move(maybe_client.value()), std::ref(run));
           t.detach();
         } else {
-          dlog::tinfo("server", "failed to get peer with error [{}]\n",
+          DLOG_INFO("server", "failed to get peer with error [{}]\n",
                       server.LastErrorToString());
         }
       } else {
-        dlog::tinfo("server", "failed to accept client with error [{}]\n",
+        DLOG_INFO("server", "failed to accept client with error [{}]\n",
                     server.LastErrorToString());
       }
     }
@@ -286,7 +286,7 @@ int main(int argc, const char** argv) {
 
   if (port < std::numeric_limits<u16>::min() ||
       port > std::numeric_limits<u16>::max()) {
-    dlog::tinfo("main", "invalid port provided, must be in the range [{}-{}]",
+    DLOG_INFO("main", "invalid port provided, must be in the range [{}-{}]",
                 std::numeric_limits<u16>::min(),
                 std::numeric_limits<u16>::max());
     return 1;
@@ -297,13 +297,13 @@ int main(int argc, const char** argv) {
   dnet::Startup();
   std::thread server_thread(RunServer, static_cast<u16>(port));
 
-  dlog::tinfo("main", "sleeping for 10 ms to let server start");
+  DLOG_INFO("main", "sleeping for 10 ms to let server start");
   std::this_thread::sleep_for(std::chrono::milliseconds(10));
   Client(static_cast<u16>(port), "127.0.0.1");
 
   server_thread.join();
 
-  dlog::tinfo("main", "sever and client closed successfully");
+  DLOG_INFO("main", "sever and client closed successfully");
 
   // windows will instantly close the terminal window, prevent that
 #ifdef DNET_PLATFORM_WINDOWS
